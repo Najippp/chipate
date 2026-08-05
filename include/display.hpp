aaa3b1d17837e3a21e0fd7bbf9d121a0f255f47d
Najippp <hilmikistijantoro@gmail.com>
2026-08-05 08:49:49 +0700

    feat: made display to render pixels based on 2D array

--- include/display.hpp
+++ include/display.hpp
@@ -8,6 +8,9 @@ private:
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Event event;
+    SDL_Texture *texture;
+
+    unsigned char pixels[32][64] = {0};
 
 public:
     bool is_running = true;
--- src/display.cpp
+++ src/display.cpp
@@ -1,8 +1,10 @@
 #include "display.hpp"
 #include <iostream>
+#include <cstring>
 
 bool Display::Init() {
     // Initialize everything needed for SDL
+    pixels[20][10] = 1;
 
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         std::cerr << "Error Initializing SDL: " << SDL_GetError() << std::endl;
@@ -24,6 +26,34 @@ bool Display::Init() {
     // Make the 960 x 480 window be treated as a 64 x 32 pixel screen
     SDL_SetRenderLogicalPresentation(renderer, 64, 32, SDL_LOGICAL_PRESENTATION_STRETCH);
 
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_INDEX8, 
+        SDL_TEXTUREACCESS_STREAMING, 64, 32);
+    if (!texture) {
+        std::cerr << "Error creating texture: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    // Link texture with a black and white color palette
+    SDL_Palette *color_palette = SDL_CreatePalette(2);
+    if (!color_palette) {
+        std::cerr << "Error creating color palette: " << SDL_GetError() << std::endl;
+        return false;
+    }
+    
+    SDL_Color colors[2] = {
+        {0, 0, 0, 255},
+        {255, 255, 255, 255}
+    };
+
+    SDL_SetPaletteColors(color_palette, colors, 0, 2);
+    SDL_SetTexturePalette(texture, color_palette);
+
+    SDL_DestroyPalette(color_palette);
+    color_palette = nullptr;
+
+    // Set scalemode so SDL does't smooth corners
+    SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_PIXELART);           
+
     std::cout << "SDL Initialized" << std::endl;
     return true;
 }
@@ -40,14 +70,26 @@ void Display::Free() {
         renderer = nullptr;
     }
 
+    if (texture) {
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
+    }
+
     SDL_Quit();
 }
 
 void Display::Render() {
+    void *draw_loc;             // Address that CPU and GPU can access
+    int pitch;
+
+    if (SDL_LockTexture(texture, NULL, &draw_loc, &pitch)) {
+        SDL_memcpy(draw_loc, pixels, sizeof(pixels));
+        SDL_UnlockTexture(texture);
+    }
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderPoint(renderer, 0, 0);
+    SDL_RenderTexture(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
 }
 
